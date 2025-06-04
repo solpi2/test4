3346addca2476724641e807c4082de79ed4cb33d
solpi2 <parkchini@gmail.com>
2025-06-04 11:19:47 +0900

    for문 커밋2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-       for (int i = 1; i <= 5; i++) {
-           System.out.println(i);
+       int number = 5;
+
+       for (int i = 1; i <= 9; i++) {
+           System.out.println(number + " x " + i + " = " + (number * i));
        }
     }
 }
