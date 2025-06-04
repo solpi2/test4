2dfa7072176409000c758965d9a949429518798d
solpi2 <parkchini@gmail.com>
2025-06-04 13:34:20 +0900

    이중 for문 구구단

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 20; i >= 1; i--) {
-            if (i % 4 == 0) { // 2, 4, 6, 8, 10
-                System.out.println("i 는 " + i);
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
             }
         }
     }
