7729637448a2aaf3788ba2688b4d8f8fcfe7fa30
solpi2 <parkchini@gmail.com>
2025-06-04 11:42:42 +0900

    for문 커밋4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i % 2 == 0) { // 2, 4, 6, 8, 10
+        for (int i = 20; i >= 1; i--) {
+            if (i % 4 == 0) { // 2, 4, 6, 8, 10
                 System.out.println("i 는 " + i);
             }
         }
