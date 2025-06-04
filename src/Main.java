aca2ebd14e20f51d4d66fdd325b527456d139ba9
solpi2 <parkchini@gmail.com>
2025-06-04 11:05:33 +0900

    for문 커밋1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 7;
-
-        System.out.println("a == b : " + ( a == b));
-        System.out.println("a != b : " + ( a != b));
-
-        System.out.println("a > b : " + ( a > b));
-        System.out.println("a < b : " + ( a < b));
-        System.out.println("a >= 10 : " + (a >= 10));
-        System.out.println("b <= 5 : " + (b <= 5));
+       for (int i = 1; i <= 5; i++) {
+           System.out.println(i);
+       }
     }
 }
